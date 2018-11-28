fe2137cbb0b550420c7d2b6c5f31a2db9b32d3c1
Matheus Feo <matheuspcsantos@gmail.com>
2018-11-28 13:15:54 -0200

    REFATORANDO ADDRESSBOOK/DELETE CODIGO REDUNTANTE

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -1,11 +1,12 @@
 describe('Address Book', function() {
+  var addressBook, thisContact;
+
+  beforeEach(function () {
+    addressBook = new AddressBook();
+    thisContact = new Contact();
+  });
 //Teste de estar apto a adicionar um contato
   it('esta apto a adicionar um contato', function() {
-    //criando instancia de Address AddressBook
-    var addressBook = new AddressBook();
-    //criando instancia de Contact
-    var thisContact = new Contact();
-
     //add contato
     addressBook.addContact(thisContact);
 
@@ -14,9 +15,6 @@ describe('Address Book', function() {
   });
   //Teste para deletar um contato da agenda
   it('esta apto a deletar um contato da agenda', function() {
-    var addressBook = new AddressBook(),
-        thisContact = new Contact();
-
     addressBook.addContact(thisContact);
     addressBook.deleteContact(0);
 
